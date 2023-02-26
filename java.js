5ef2f430c279c3129eb3fcada161baac508e0eb4
najm-choueiry <nachoueiry@outlook.com>
2023-02-26 11:11:29 +0200

    ids

--- java.js
+++ java.js
@@ -1,9 +1,6 @@
-window.onload = function() {
 
-    var string ;
 
-    let full_name = document.getElementById("full_name")
-    console.log(full_name)
+let full_name = document.getElementById("full_name").value; 
+console.log(full_name)
+
     
-    
-}
